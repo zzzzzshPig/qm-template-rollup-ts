c931930627a1b9ea2fa0bbc150a20f4c176a9867
zhoushaohui <1012352205@qq.com>
2021-03-17 10:17:46 +0800

    Feat(login): 测试地址修改

--- src/main.ts
+++ src/main.ts
@@ -51,7 +51,7 @@ class Login {
     }
 
     login () {
-        const url = this.env === 'production' ? 'https://admin.91xiangju.com/frontend-platform/login' : 'http://192.168.1.160:8001/login'
+        const url = this.env === 'production' ? 'https://admin.91xiangju.com/frontend-platform/login' : 'http://192.168.1.66/frontend-platform-dev/login'
         const from = `qm_from=${encodeURIComponent(location.href)}`
         const title = `&title=${encodeURIComponent(this.title)}`
         openWin(`${url}?${from}${title}`, this.target)
