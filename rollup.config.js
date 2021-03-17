4fe61e7532e99c5e1a08413ededc2f4d2d534667
zhoushaohui <1012352205@qq.com>
2021-03-17 10:57:05 +0800

    Fix(build): 修复esm文件没有打包模块

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import nodePolyfills from 'rollup-plugin-polyfill-node'
 
 function baseConfig () {
     return {
@@ -11,8 +12,7 @@ function baseConfig () {
         },
         plugins: [
             terser(),
-            typescript(),
-            nodeResolve()
+            typescript()
         ],
         external: []
     }
@@ -20,11 +20,21 @@ function baseConfig () {
 
 const umd = baseConfig()
 umd.output.format = 'umd'
-umd.output.file = 'dist/index.js'
+umd.output.file = 'dist/index.browser.js'
+umd.plugins.push(
+    nodeResolve({
+        browser: true,
+        preferBuiltins: false
+    }),
+    nodePolyfills()
+)
 
 const esm = baseConfig()
 esm.output.format = 'esm'
 esm.output.file = 'dist/index.esm.js'
+esm.plugins.push(
+    nodeResolve()
+)
 
 export default [
     umd,
