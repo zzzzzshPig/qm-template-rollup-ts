04576ffa904f7ae7285e28b4e46a01649a40e8f0
zhoushaohui <1012352205@qq.com>
2021-03-29 11:32:40 +0800

    Feat: 测试

--- prompt.js
+++ prompt.js
@@ -2,37 +2,40 @@ const prompt = require('prompt')
 const shell = require('shelljs')
 const package = require('./package.json')
 
+let nextVersion = package.version.split('.')
+nextVersion[nextVersion.length - 1] = Number(nextVersion[nextVersion.length - 1]) + 1
+nextVersion = nextVersion.join('.')
+
 async function init () {
     prompt.start()
 
-    const config = {
-        version: {
-            properties: {
-                version: {
-                    message: `当前版本${package.version}`,
-                    required: true
-                }
-            }
-        },
-        commit: {
-            properties: {
-                commit: {
-                    message: 'git commit log',
-                    required: true
-                }
+    const _version = {
+        properties: {
+            version: {
+                message: `当前版本${package.version}`,
+                default: nextVersion,
+                required: true
             }
         }
     }
 
-    const { version } = await prompt.get(config.version)
+    const { version } = await prompt.get(_version)
     shell.echo('rollup building......')
     shell.exec('rollup -c')
 
-    shell.echo('input git commit')
-    const { commit } = await prompt.get(config.commit)
-    shell.exec(`git commit -m ${commit}`)
+    const _commit = {
+        properties: {
+            commit: {
+                message: 'git commit log',
+                default: `Update: ${version}`,
+                required: true
+            }
+        }
+    }
 
-    shell.echo(`生成${version}版本`)
+    shell.echo('input git commit')
+    const { commit } = await prompt.get(_commit)
+    shell.exec(`git commit -m ${commit}`)
     shell.exec(`npm version ${version}`)
 }
 
