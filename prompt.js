e37efc073ce2818c268ad0e6c655f9a98ce20d14
zhoushaohui <1012352205@qq.com>
2021-03-29 11:26:04 +0800

    Feat: 测试

--- prompt.js
+++ prompt.js
@@ -1,20 +1,38 @@
 const prompt = require('prompt')
 const shell = require('shelljs')
+const package = require('./package.json')
 
 async function init () {
     prompt.start()
 
     const config = {
-        properties: {
-            version: {
-                message: '版本号',
-                required: true
+        version: {
+            properties: {
+                version: {
+                    message: `当前版本${package.version}`,
+                    required: true
+                }
+            }
+        },
+        commit: {
+            properties: {
+                commit: {
+                    message: 'git commit log',
+                    required: true
+                }
             }
         }
     }
 
-    const { version } = await prompt.get(config)
-    shell.echo(`开始构建${version}版本`)
+    const { version } = await prompt.get(config.version)
+    shell.echo('rollup building......')
+    shell.exec('rollup -c')
+
+    shell.echo('input git commit')
+    const { commit } = await prompt.get(config.commit)
+    shell.exec(`git commit -m ${commit}`)
+
+    shell.echo(`生成${version}版本`)
     shell.exec(`npm version ${version}`)
 }
 
